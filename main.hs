afa6bbab018ba8af5ae15b2cf57f8e18286ee1a1
L Mangu <cs16btech11021@iith.ac.in>
2018-04-28 00:37:07 +0530

    Add Screen

---
+++ main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Graphics.Gloss
+import Graphics.Gloss.Data.Color
+
+import Game
+
+window = InWindow "Functional" (screenWidth, screenHeight) (100, 100)
+backgroundColor = makeColor 0 0 0 255
+
+main :: IO ()
+main = play window backgroundColor 30 initialGame gameAsPicture transformGame (const id)
