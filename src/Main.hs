7147f72f7cc75ee10fe09c3ce13ae248315962fe
UmaReddy <cs16btech11033@iith.ac.in>
2018-04-28 18:02:38 +0530

    Initiates the game importing other modules

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,11 @@
 module Main where
-
 import Graphics.Gloss
 import Graphics.Gloss.Data.Color
-
 import Game
 import Board
 import Logic
-
+-- |window sets the screenWidth and screenheight and also the background colour 
 window = InWindow "Functional" (screenWidth, screenHeight) (100, 100)
 backgroundColor = makeColor 0 0 0 255
-
 main :: IO ()
 main = play window backgroundColor 30 initialGame gameAsPicture transformGame (const id)
